678e603041bd56e816cf1c881a0c2469e9656728
Ayşegül_Orhun <orhunaysegul0@gmail.com>
2026-02-08 01:20:29 +0300

    v1.0.2 version

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,11 +6,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true
       }
     }
+  },
+  preview: {
+    port: 5173,
+    host: true
   }
 })
